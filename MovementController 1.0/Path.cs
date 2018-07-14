777ef272c8ac3646c2bdb9bb5feed835203a5e16
Travis <super@DESKTOP-62TLF66>
2018-07-13 21:16:24 -0400

    Added a Path class, but didn't use it yet.

---
+++ MovementController 1.0/Path.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovementController_1._0
+{
+    /// <summary>
+    /// Not used yet, just a start
+    /// </summary>
+    class Path
+    {
+        public List<DiscreteCommand> trajectory { get; set; }
+        public DiscreteCommand finalCommand { get; set; }
+
+        public Path()
+        {
+            trajectory = new List<DiscreteCommand>();
+            finalCommand = new DiscreteCommand(new AZELCoordinate(0, 0), 0);
+        }
+    }
+}
