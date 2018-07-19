5d9953c412cf989ad6f77537d8fed1bc33023e63
fdiaz3 <fdiaz@ycp.edu>
2018-07-19 06:54:27 -0400

    Added motor controller file and unity file contents, made placholder for PLC

--- MovementController 1.0/MotorController.cs
+++ MovementController 1.0/MotorController.cs
@@ -3,11 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
 
 namespace MovementController_1._0
 {
 	class MotorController
 	{
-		
+		SerialPort port;
+
+		public MotorController()
+		{
+			
+		}
+
+		public void Init(string portName, int baud)
+		{
+			port = new SerialPort();
+			port.PortName = portName;
+			port.BaudRate = baud;
+
+			try
+			{
+				port.Open();
+			}
+			catch(Exception error)
+			{
+				throw error;
+			}
+		}
+
+		public void moveAbsolute(decimal cmd)
+		{
+			if (port.IsOpen)
+			{
+				port.WriteLine(cmd.ToString());
+			}
+			else
+			{
+				throw new Exception("port is not open!");
+			}
+		}
 	}
 }
--- MovementController 1.0/UnityController.cs
+++ MovementController 1.0/UnityController.cs
@@ -8,5 +8,45 @@ namespace MovementController_1._0
 {
 	class UnityController
 	{
+		//static byte[] bytes = new byte[256];
+		static System.IO.StreamWriter sentFile = new System.IO.StreamWriter(@"C:\Users\Public\Driver\data.txt", true);
+		static System.IO.StreamReader receiveFile = new System.IO.StreamReader(@"C:\Users\Public\Driver\limits.txt", true);
+
+		public static void sendData(decimal val)
+		{
+			sentFile.WriteLine(val);
+			/*IPAddress host = IPAddress.Parse("192.168.1.18");
+            IPEndPoint hostep = new IPEndPoint(host, 80);
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            byte[] msg = Encoding.UTF8.GetBytes(val.ToString());
+            byte[] bytes = new byte[256];
+            try
+            {
+                // Blocks until send returns.
+                int i = sock.Send(msg);
+                Console.WriteLine("Sent {0} bytes.", i);
+
+                // Get reply from the server.
+                i = sock.Receive(bytes);
+                Console.WriteLine(Encoding.UTF8.GetString(bytes));
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("{0} Error code: {1}.", e.Message, e.ErrorCode);
+            }*/
+		}
+		public static string RecieveData()
+		{
+			string data = receiveFile.ReadToEnd();
+
+			return data;
+			/*IPAddress host = IPAddress.Parse("192.168.1.18");
+            IPEndPoint hostep = new IPEndPoint(host, 80);
+            Socket sock1 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            sock1.Receive(bytes);
+            Console.WriteLine(Encoding.UTF8.GetString(bytes));*/
+		}
 	}
 }
