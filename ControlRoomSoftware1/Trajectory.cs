2c99589e8c88d6812968898b196e757216847203
Nick Vandemark <rvandemark@ycp.edu>
2018-07-26 14:48:18 -0400

    Stable state for trajectory, further improvements still to be made.

--- ControlRoomSoftware1/Trajectory.cs
+++ ControlRoomSoftware1/Trajectory.cs
@@ -8,7 +8,10 @@ namespace ControlRoomSoftware1
 {
     public interface Trajectory
     {
+        // Use any logic to create characteristics that are necessary for EvaluateAtElapsedTime()
         void Generate();
+
+        // Given an elapsed time since trajectory start, find and return a reference position
         double EvaluateAtElapsedTime(double dt);
     }
 
@@ -19,9 +22,14 @@ namespace ControlRoomSoftware1
         private double initialVelocity;
         private double initialAcceleration;
         private double objectivePositon;
+
+        // The total amount of time that should pass between start and finish
+        // If it should happen as fast as possible, this should be set to a value
+        // less than 0 before Generate() is called
         private double elapsedTime;
 
-        // Internal tracker for negative change in position
+        // Internal tracker for negative change in position, allows for easier
+        // calculation down the line, both visually and computationally
         private bool isNegative;
 
         // Values that dictate the peak velocity of the move, either input by the
@@ -48,8 +56,13 @@ namespace ControlRoomSoftware1
         public TrajectoryProfile(double xi, double vi, double ai, double xf, double et, double pv, double pa)
         {
             initialPosition = xi;
+
+            // Assumption
             initialVelocity = 0;
+
+            // Assumption
             initialAcceleration = 0;
+
             objectivePositon = xf;
             elapsedTime = et;
 
@@ -62,25 +75,99 @@ namespace ControlRoomSoftware1
         public void Generate()
         {
             // Assume non-synced end times
-            double dx = objectivePositon - initialPosition;
-            isNegative = dx < 0;
+            double displacement = objectivePositon - initialPosition;
+            isNegative = displacement < 0;
 
             // Get magnitude of change in position
-            dx = Math.Abs(dx);
+            double distanceTraveled = Math.Abs(displacement);
 
-            // Get ideal acceleration, velocity, and position changes
+            // Calculate the important time segments
             // TODO: change this to not only account for best-case scenario
             double t1 = peakAcceleration / References.SYSTEM_JERK;
             double t2 = peakVelocity / peakAcceleration;
-            double t4 = elapsedTime - (4 * t1) - (2 * t2);
+            
+            double t4;
+            if (elapsedTime < 0)
+            {
+                // Move as fast as legally allowed
+                t4 = distanceTraveled / peakVelocity;
+            }
+            else
+            {
+                // Fill up the remainder of our time with the constant velocity
+                // TODO: verify this math is correct with simulation
+                t4 = elapsedTime - (4 * t1) - (2 * t2);
+            }
 
             // Discretize the times, guarantee our maximum values aren't met
-            double t1Disc = Math.Round(t1, 3);
-            double t2Disc = Math.Round(t2, 3);
-            double t4Disc = Math.Round(t4, 3);
+            double disc_t1 = Math.Round(t1, 3);
+            double disc_t2 = Math.Round(t2, 3);
+            double disc_t4 = Math.Round(t4, 3);
 
-            // Override the peak values to reflect the discretization
+            // In the ideal scenario, we now know all of our t values to define the
+            // entire profile
+            t[0] = disc_t1;
+            t[1] = disc_t2;
+            t[2] = disc_t1 + disc_t2;
+            t[3] = disc_t4;
+            t[4] = disc_t4 + disc_t1;
+            t[5] = disc_t4 + disc_t2;
+            t[6] = disc_t4 + disc_t1 + disc_t2;
 
+            // Override the peak values to reflect the discretization, guarantees
+            // that it will be below the specified maxmimum values
+            discJerk = peakAcceleration / disc_t1;
+            discAcceleration = peakVelocity / disc_t2;
+            discVelocity = distanceTraveled / disc_t4;
+
+            // Calculate all of our intermediate reference values
+            // Get the reference position and velocity after the accelerating portion of bring-up
+            intermediatePos[0] = Kinematics.PositionAt(
+                initialPosition, 0, 0, discJerk, t[0]
+            );
+            intermediateVel[0] = Kinematics.VelocityAt(
+                0, 0, discJerk, t[0]
+            );
+            
+            // Get the reference position and velocity after the constant acceleration portion of bring-up
+            intermediatePos[1] = Kinematics.PositionAt(
+                intermediatePos[0], intermediateVel[0], discAcceleration, 0, t[1] - t[0]
+            );
+            intermediateVel[1] = Kinematics.VelocityAt(
+                intermediateVel[0], discAcceleration, 0, t[1] - t[0]
+            );
+
+            // Get the reference position and velocity after the decelerating portion of bring-up
+            intermediatePos[2] = Kinematics.PositionAt(
+                intermediatePos[1], intermediateVel[1], discAcceleration, -discJerk, t[2] - t[1]
+            );
+            intermediateVel[2] = Kinematics.VelocityAt(
+                intermediateVel[1], discAcceleration, -discJerk, t[2] - t[1]
+            );
+
+            // Get the reference position and velocity during the constant velocity sequence
+            intermediatePos[3] = Kinematics.PositionAt(
+                intermediatePos[2], intermediateVel[2], 0, 0, t[3] - t[2]
+            );
+            intermediateVel[3] = Kinematics.VelocityAt(
+                intermediateVel[2], 0, 0, t[3] - t[2]
+            );
+
+            // Get the reference position and velocity after the accelerating portion of bring-down
+            intermediatePos[4] = Kinematics.PositionAt(
+                intermediatePos[3], intermediateVel[3], 0, -discJerk, t[4] - t[3]
+            );
+            intermediateVel[4] = Kinematics.VelocityAt(
+                intermediateVel[3], 0, -discJerk, t[4] - t[3]
+            );
+
+            // Get the reference position and velocity after the constant acceleration portion of bring-down
+            intermediatePos[5] = Kinematics.PositionAt(
+                intermediatePos[4], intermediateVel[4], -discAcceleration, 0, t[5] - t[4]
+            );
+            intermediateVel[5] = Kinematics.VelocityAt(
+                intermediateVel[4], -discAcceleration, 0, t[5] - t[4]
+            );
         }
 
         public double EvaluateAtElapsedTime(double dt)
